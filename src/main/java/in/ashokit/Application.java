c6d80aa931db0bfac60d40704387edeb0337b7c3
BhalchandraNalawade <bhalchandranalawade12@gmail.com>
2026-04-13 18:27:08 +0530

    first commit

---
+++ src/main/java/in/ashokit/Application.java
@@ -0,0 +1,21 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.ashokit.dao.UserDao;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		
+	ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+	
+	UserDao bean = context.getBean(UserDao.class);
+	System.out.println(bean.hashCode());
+
+	}
+
+} 
